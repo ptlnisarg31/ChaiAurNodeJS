06d0293aa03fd9c4c9fe6d67f3ab9076b24e6185
nisarg <zoomtest4u@gmail.com>
2024-01-09 22:49:33 +0530

    new

---
+++ SecNode/Backend/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+const port = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening at port ${port}`);
+});
