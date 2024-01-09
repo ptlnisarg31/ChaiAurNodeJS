ed3551418e10e0edd30149f793098b8051ff8686
nisarg <zoomtest4u@gmail.com>
2024-01-10 01:09:52 +0530

    last

---
+++ SecNode/Backend/models/ecommerce/user.models.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      reqired: true,
+      unique: true,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      reqired: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      reqired: true,
+    },
+  },
+  { timestamps: true }
+);
+export const User = mongoose.model("User", userSchema);
--- SecNode/Backend/models/todod/todo.models.js
+++ SecNode/Backend/models/todod/todo.models.js
@@ -1,3 +1,26 @@
 import mongoose from "mongoose";
-const todoSchema = new mongoose.Schema({}, { timestamps: true });
+const todoSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    complete: {
+      type: Boolean,
+      default: false,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    subType: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "SubTodo",
+      },
+    ], //Array of Subtodos
+  },
+  { timestamps: true }
+);
 export const Todo = mongoose.model("Todo", todoSchema);
